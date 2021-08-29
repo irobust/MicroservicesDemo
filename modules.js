4e3ca66791c9143657e7c72b1b1a8a6e0a0904bc
Phanupong Permpimol <phanupong@Phanupongs-MacBook-Pro.local>
2021-08-29 23:37:57 +0700

    edit readme

--- modules.js
+++
@@ -1,9 +0,0 @@
-var products = require('./product-service.js')();
-var deliveries = require('./delivery-service.js')();
-module.exports = () => {
-    return {
-        products: products,
-        deliveries: deliveries
-    }
-}
-
