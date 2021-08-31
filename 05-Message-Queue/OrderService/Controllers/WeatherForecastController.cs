656a0564b27889a38bc649a3c6efb3536456ead2
Phanupong Permpimol <phanupong@Phanupongs-MacBook-Pro.local>
2021-08-31 14:55:52 +0700

    update dotnet example

--- 05-Message-Queue/OrderService/Controllers/WeatherForecastController.cs
+++ 05-Message-Queue/OrderService/Controllers/WeatherForecastController.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 
 namespace OrderService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class OrderController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<OrderController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public OrderController(ILogger<OrderController> logger)
         {
             _logger = logger;
         }
@@ -27,13 +25,32 @@ namespace OrderService.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = rng.Next(-20, 55)
             })
             .ToArray();
+        }
+
+         [HttpPost]
+        public Object Create(){
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://webapp:rabbitmq@host.docker.internal:5672");
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var message = "Create new order";
+            var bytes = Encoding.UTF8.GetBytes(message);
+
+            channel.QueueDeclare("demo.order", true, false, false);
+            channel.BasicPublish("", "demo.order", null, bytes);
+
+            channel.Close();
+            connection.Close();
+            return new { success = true };
         }
     }
 }
